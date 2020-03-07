4726e46835449f03e3c3594396f858b6b7892f11
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2020-03-07 12:27:39 +0100

    Add nice loading indicator for expenses list

--- src/components/expenses/components/expenses-list/expenses-list.tsx
+++ src/components/expenses/components/expenses-list/expenses-list.tsx
@@ -1,13 +1,27 @@
 import React, { Fragment } from 'react'
-import { NewExpense, SavedExpense } from '../expense'
 import { useSelector } from 'react-redux'
-import { expensesReceipts } from '../../expenses.selectors'
+import { Divider, Header, Loader } from 'semantic-ui-react'
+import { FormattedMessage } from 'react-intl'
+
+import { NewExpense, SavedExpense } from '../expense'
+import { expensesLoading, expensesReceipts } from '../../expenses.selectors'
+
+import styles from './expenses-list.module.css'
 
 export const ExpensesList = () => {
   const receipts = useSelector(expensesReceipts)
+  const loading = useSelector(expensesLoading)
 
   return (
     <Fragment>
+      {loading && (
+        <Divider horizontal>
+          <Header as="h6">
+            <Loader active inline className={styles.dividerLoader} size="tiny" />
+            <FormattedMessage id="expenses.loading-new-items" />
+          </Header>
+        </Divider>
+      )}
       <NewExpense />
       {receipts.map(receipt => (
         <SavedExpense key={receipt.id} receipt={receipt} />
