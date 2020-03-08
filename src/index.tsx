06ab28dd24935cfe51bce7172e6106e479bee969
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2020-03-08 18:58:06 +0100

    Add Rollbar support

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Rollbar from 'rollbar'
 
 // Intl polyfills for IE11, Edge and Safari
 import '@formatjs/intl-pluralrules/polyfill'
@@ -13,10 +14,19 @@ import { App } from './app'
 import * as serviceWorker from './serviceWorker'
 import { initEncryption } from './encryption/encryption'
 
+new Rollbar({
+  accessToken: '3b2f756af2df4fcdb03ec540f63e1d8f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: process.env.NODE_ENV,
+  },
+})
+
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
-initEncryption();
+initEncryption()
